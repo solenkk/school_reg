19afc6c42b0d86f481c79df706a03e88dfa5db41
solenkk <kassahunsolen@gmail.com>
2025-10-15 00:35:07 +0300

    ks

--- ap/serilizer.py
+++ ap/serilizer.py
@@ -1,11 +1,21 @@
 from rest_framework import serializers
-from .models import StudentProfile, Course
+from .models import StudentProfile, Course, Enrollment
 
 class StudentProfileserializer(serializers.ModelSerializer):
+    model= StudentProfile
     class Meta:
-        model= StudentProfile
         fields= ('student_id','id','department','fristname','lastname')
+
 class Courseserilserializer(serializers.Modelserializer):
+    model=Course
     class Meta:
-        
-        feild
+        student_identity=StudentProfileserializer(source='StudentProfile' , read_only=True)
+
+        feild=('title','code', 'credit_hour', 'instructor','id')
+
+class EnrollmentSerialzer(serializers.Modelserializer):
+    model=Enrollment
+    stuedent_identity= StudentProfileserializer(source='studentprofile', read_only=True)
+    class Meta:
+    
+        fields=('date_enrolled','student','course','id')
